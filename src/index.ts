0d5467fec2d66a8327b7bea2fb493016baf6dd38
FranciscoVeloz <franciscoveloz245@gmail.com>
2022-11-09 18:38:09 -0600

    Fixed card import

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { s } from "./styles";
 import Button from "./components/Button";
-import Card from "./components/Button";
+import Card from "./components/Card";
 import Container from "./components/Container";
 import Input from "./components/Input";
 import Shadow from "./components/Shadow";
